41f78dbe145cb4eda834c459836856e51a22559b
MAD71241 <michele.deliso@gmail.com>
2021-06-28 15:47:16 +0200

    lavoro a struttura ad oggetti per e-shop

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+class Product {
+    private int $id;
+    private string $name;
+    private float $price;
+    private int $qty;
+    
+    function __construct($id, $name, $price, $qty)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->qty = $qty;
+    }
+}
+
+class Cart {
+    private array $items = [];
+
+    public function addProduct(Product $product, int $quantity) {
+        $cartItem = new CartItem;
+    }
+}
+
+class CartItem {
+
+}
