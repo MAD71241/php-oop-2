2d46dbeeacd085b4b26e65b3e23ad41374d1e62a
MAD71241 <michele.deliso@gmail.com>
2021-06-28 17:14:44 +0200

    rimodulate classi, aggiunto credit card

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 class Product {
-    private int $id;
-    private string $name;
-    private float $price;
-    private int $qty;
+    protected int $id;
+    protected string $name;
+    protected float $price;
+    protected int $qty;
     
     function __construct($id, $name, $price, $qty)
     {
@@ -13,17 +13,95 @@ class Product {
         $this->price = $price;
         $this->qty = $qty;
     }
-}
 
-class Cart {
-    private array $items = [];
+    public function getId () {
+        return $this->id;
+    }
 
-    public function addProduct(Product $product, int $quantity) {
-        $cartItem = new CartItem;
+    public function getName () {
+        return $this->name;
+    }
+
+    public function getQty () {
+        return $this->qty;
     }
 }
 
+class Television extends Product {
+
+    function __construct($id, $name, $price, $qty, $inches, $videotech)
+    {
+        parent::__construct($id, $name, $price, $qty);
+        $this->inches = $inches;
+        $this->videotech = $videotech;
+    }
+}
+
+$tcl = new Television(12, "TCL Silver", 290, 1, 55, "OLED");
+var_dump($tcl);
+
+/* USERS */
+
+class User {
+    protected string $name;
+    public string $username;
+    protected string $payment_method;
+    protected string $gender;
+    protected string $language;
+
+    function __construct($name, $username, $payment_method, $gender, $language)
+    {
+        $this->name = $name;
+        $this->username = $username;
+        $this->payment_method = $payment_method;
+        $this->gender = $gender;
+        $this->language = $language;
+    }
+}
+
+/* CREDIT-CARD */
+class CreditCard
+{
+    protected $owner_name;
+    protected $owner_lastname;
+    protected $number;
+    protected $deadline;
+    protected $cvc;
+
+    public function __construct(string $name, string $lastname, int $number, int $deadline, int $cvc ) {
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->number = $number;
+        $this->deadline = $deadline;
+        $this->cvc = $cvc;
+    }
+}
+
+/* CART */
+class Cart {
+    private array $items = [];
+
+    public function addProduct(Product $product, int $itemquantity) {
+    }
+}
+
+
+/* CART-ITEM */
 class CartItem {
     private Product $product;
     private int $quantity;
+
+    function __construct(Product $product, int $givenquantity)
+    {
+        $this->product = $product;
+        $this->quantity = $givenquantity;
+    }
+
+    function getGivenQty () {
+        return $this->givenquantity;
+    }
+
+    function setQty (int $givenquantity) {
+        $this->quantity = $givenquantity;
+    }
 }
